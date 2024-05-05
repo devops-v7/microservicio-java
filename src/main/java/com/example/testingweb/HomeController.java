ead5c612ba0e76bb9bb1109bd671b84b1268d10e
Rodrigo Orellana <rodrigo.orelana@gmail.com>
2024-05-05 17:59:32 -0400

    chore: remueve variable password y agrega TODO

--- src/main/java/com/example/testingweb/HomeController.java
+++ src/main/java/com/example/testingweb/HomeController.java
@@ -9,7 +9,7 @@ public class HomeController {
 
 	@RequestMapping("/")
 	public @ResponseBody String greeting() {
-		String password = "secret";
+		//TODO cambiar el retorno
 		return "Hello, World";
 	}
 
