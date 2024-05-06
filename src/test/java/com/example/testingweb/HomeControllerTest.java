2403c7243a1fdd89a9853000e3189dab31bddb75
Rodrigo Orellana <rodrigo.orelana@gmail.com>
2024-05-06 09:14:18 -0400

    chore: arregla HomeControllerTest

--- src/test/java/com/example/testingweb/HomeControllerTest.java
+++ src/test/java/com/example/testingweb/HomeControllerTest.java
@@ -1,10 +1,13 @@
+package com.example.testingweb;
+
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -15,7 +18,9 @@ public class HomeControllerTest {
 
     @Test
     void testGreeting() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+        // Verify if the "/" endpoint returns the correct greeting message
+        mockMvc.perform(get("/"))
+               .andExpect(status().isOk())
                .andExpect(content().string("Hola, soy Rodrigo Orellana"));
     }
 }
